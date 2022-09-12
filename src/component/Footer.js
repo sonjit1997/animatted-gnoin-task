73f838dd58adec9f5684fdfa0005848621581433
Sonjit Saha <sonjitsaha78@gmail.com>
2022-09-12 16:34:30 +0530

    latest commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -18,18 +18,18 @@ const Footer = () => {
       </button>
 
       <div
-        class="offcanvas offcanvas-bottom"
+        className="offcanvas offcanvas-bottom"
         tabIndex="-1"
         id="offcanvasBottom"
         aria-labelledby="offcanvasBottomLabel"
       >
-        <div class="offcanvas-body small">
-          <div class="container text-center">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-              <div class="col text  display-6">Support </div>
-              <div class="col text  display-6">Oficelink </div>
-              <div class="col text  display-6">Newsletter</div>
-              <div class="col text  display-6">Social </div>
+        <div className="offcanvas-body small">
+          <div className="container text-center">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+              <div className="col text  display-6">Support </div>
+              <div className="col text  display-6">Oficelink </div>
+              <div className="col text  display-6">Newsletter</div>
+              <div className="col text  display-6">Social </div>
             </div>
           </div>
         </div>
